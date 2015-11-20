c1396e5aedf9cc41e1060955fa5ff8cd80c53923
Giovazz89 <giovazz89@gmail.com>
2015-11-20 09:15:40 +0100

    setting plugin structure

--- enhanced-val.js
+++ enhanced-val.js
@@ -1,9 +1,43 @@
 (function ($) {
   var originalVal = $.fn.val;
-  $.fn.val = function(value) {
+  
+  $.fn.val = function(value, resetField = true) {
+    
+    /* if specified value set it
+    ---------------------------------------------------------
+    */
     if (typeof value != 'undefined') {
-      // setter invoked, do processing
+      // make the passed value an array in any case
+      if(!$.isArray(value)) value = [value];
+      
+      // apply value to all passed elements
+      $(this).each(funtion(){
+        $(this).setSingleVal(value, resetField);
+      });
+      
+      return;
     }
-    return originalVal.call(this, value);
+    
+    /* if not specified value return
+    ---------------------------------------------------------
+    */
+    var returnValues = [];
+    
+    // get value from all passed elements
+    $(this).each(funtion(){
+      returnValues.push($(this).setSingleVal());
+    });
+    
+    if(returnValues.length == 0) return;
+    if(returnValues.length == 1) returnValues = returnValues[0];
+    return returnValues;
+  };
+  
+  $.fn.getSingleVal = function() {
+    var element = $(this);
+  };
+  
+  $.fn.setSingleVal = function(value, resetField) {
+    var element = $(this);
   };
 })(jQuery);

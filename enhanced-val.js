6ecc6ede4fb45a95e04a314ad7ba1f3063d3c15e
Giovazz89 <giovazz89@gmail.com>
2015-11-21 10:11:55 +0100

    Improved functions management
    Improved functions management, bug fix and example HTML update

--- enhanced-val.js
+++ enhanced-val.js
@@ -1,78 +1,95 @@
 (function ($) {
-  var originalVal = $.fn.val;
-  
-  var customVals = {
-    inputtext: function(element, values, resetField = true){
-      if (typeof values != 'undefined')
-        for(var i in values){
-          var newVal = values[i];
-          if(!resetField) newVal = element.originalVal() + newVal;
-          element.originalVal(newValue);
+    var originalVal = $.fn.val;
+
+    var customVals = {
+        inputtext: function(element, values, resetField){
+            if (typeof values != typeof undefined)
+                for(var i in values){
+                    var newValue = values[i];
+                    if(!resetField)
+                        newValue = element[0].value + newValue;
+                    element[0].value = newValue;
+                }
+            else
+                return element[0].value;
+        },
+        inputcheckbox: function(element, values, resetField){
+            if (typeof values != typeof undefined){
+                if(resetField)
+                    element[0].checked = false;
+                for(var i in values)
+                    if(values[i] == element[0].value)
+                        element[0].checked = true;
+            }
+            else if(element[0].checked == false) return null;
+            else return element[0].value;
+        },
+
+        other: function(element, values, resetField){
+            if (typeof values != typeof undefined)
+                for(var i in values){
+                    var newValue = values[i];
+                    if(!resetField) newValue = element.html() + newValue;
+                    element.html(newValue);
+                }
+            else
+                return element.html();
         }
-      else
-        return element.originalVal();
-    },
-    inputcheckbox: function(element, values, resetField = true){
-      if (typeof values != 'undefined'){
-        if(resetField)
-          element[0].checked = false;
-        for(var i in values)
-          if(values[i] == element.originalVal())
-            element[0].checked = true;
-      }
-      else if(element[0].checked == false) return null;
-      else return element.originalVal();
     }
-    
-    other: function(element, value, resetFiled = true){
-      if (typeof values != 'undefined')
-        for(var i in values){
-          var newVal = values[i];
-          if(!resetField) newVal = element.text() + newVal;
-          element.text(newValue);
+
+
+    $.fn.val = function(value, resetField) {
+        if(typeof resetField == typeof undefined) resetField = true;
+
+        /* if specified value set it
+        ---------------------------------------------------------
+        */
+        if (typeof value != typeof undefined) {
+
+            // make the passed value an array in any case
+            if(!$.isArray(value)) value = [value];
+
+            // apply value to all passed elements
+            $(this).each(function(){
+                var node = this.tagName.toLowerCase();
+                if (typeof $(this).attr('type') != typeof undefined)
+                    node += $(this).attr('type');
+                if(customVals.hasOwnProperty(node))
+                    customVals[node]($(this), value, resetField);
+                else
+                    customVals.other($(this), value, resetField);
+            });
+
+            return;
         }
-      else
-        return element.text();
-    }
-  }
-  
-  $.fn.val = function(value, resetField = true) {
-    
-    /* if specified value set it
-    ---------------------------------------------------------
-    */
-    if (typeof value != 'undefined') {
-      // make the passed value an array in any case
-      if(!$.isArray(value)) value = [value];
-      
-      // apply value to all passed elements
-      $(this).each(funtion(){
-        $(this).setSingleVal(value, resetField);
-      });
-      
-      return;
-    }
-    
-    /* if not specified value return
-    ---------------------------------------------------------
-    */
-    var returnValues = [];
-    
-    // get value from all passed elements
-    $(this).each(funtion(){
-      returnValues.push($(this).setSingleVal());
-    });
-    
-    if(returnValues.length == 0) return;
-    if(returnValues.length == 1) returnValues = returnValues[0];
-    return returnValues;
-  };
-  
-  $.fn.getSingleVal = function() {
-    var element = $(this);
-  };
-  
-  $.fn.setSingleVal = function(value, resetField) {
-    var element = $(this);
-  };
+
+        /* if not specified value return
+        ---------------------------------------------------------
+        */
+        var returnValues = [];
+
+        // get value from all passed elements
+        $(this).each(function(){
+            var node = this.tagName.toLowerCase();
+            if (typeof $(this).attr('type') != typeof undefined)
+                node += $(this).attr('type');
+            if(customVals.hasOwnProperty(node))
+                returnValues.push(customVals[node]($(this)));
+            else
+                returnValues.push(customVals.other($(this)));
+        });
+
+        if(returnValues.length == 0) return;
+        if(returnValues.length == 1) returnValues = returnValues[0];
+
+        return returnValues;
+    };
+
+    $.fn.getSingleVal = function() {
+        var element = $(this);
+    };
+
+    $.fn.setSingleVal = function(value, resetField) {
+        var element = $(this);
+    };
 })(jQuery);
